c42bdd73d7b0b1c04b1c5fc5993a9d355bdc2da2
Andrii <iluxor1991@gmail.com>
2022-12-23 15:43:30 +0200

    refactor: add reactQuery

--- app/app.tsx
+++ app/app.tsx
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import Navigation from '@/navigation'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
         <Navigation />
         <StatusBar style='auto' />
       </SafeAreaProvider>
-    </>
+    </QueryClientProvider>
   )
 }
