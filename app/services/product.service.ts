22bef77a91d2c953ad56236c501bb59a48bd8593
lastofpudge <iluxor1991@gmail.com>
2026-05-26 05:40:02 +0300

    feat: add ProductService for fakestoreapi.com

---
+++ app/services/product.service.ts
@@ -0,0 +1,17 @@
+import type { IProduct } from '@/types/product'
+
+const API_URL = process.env.EXPO_PUBLIC_API_URL
+
+export const ProductService = {
+  getProducts: (): Promise<IProduct[]> =>
+    fetch(`${API_URL}/products`).then((r) => r.json()),
+
+  getProduct: (id: number): Promise<IProduct> =>
+    fetch(`${API_URL}/products/${id}`).then((r) => r.json()),
+
+  getProductsByCategory: (category: string): Promise<IProduct[]> =>
+    fetch(`${API_URL}/products/category/${category}`).then((r) => r.json()),
+
+  getCategories: (): Promise<string[]> =>
+    fetch(`${API_URL}/products/categories`).then((r) => r.json()),
+}
