53b0a977d8affb0ec61a688b18bf99114d056964
lastofpudge <iluxor1991@gmail.com>
2026-05-26 05:37:39 +0300

    feat: migrate layout to StyleSheet

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -1,8 +1,9 @@
 import type { FC, PropsWithChildren } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { styles } from './styles'
 
 const Layout: FC<PropsWithChildren<unknown>> = ({ children }) => {
-  return <SafeAreaView className='flex-1'>{children}</SafeAreaView>
+  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>
 }
 
 export default Layout
